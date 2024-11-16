69e0e3819e22dc965fe2feea3d54edb38d9f7bcc
rejabusafchef <144821702+rejabusafchef@users.noreply.github.com>
2024-11-17 03:26:48 +0400

    replaced with while loop

--- insertionSort.js
+++ insertionSort.js
@@ -1,9 +1,10 @@
 function insertionSort(arr) {
   for (let i = 1; i <= arr.length - 1; i++) {
     let nti = arr[i];
-    let j = 0;
-    for (j = i - 1; (j >= 0) && (arr[j] > nti); j--) {
+    let j = i - 1;
+    while ((j >= 0) && (arr[j] > nti)) {
       arr[j + 1] = arr[j];
+      j--;
     }
     arr[j + 1] = nti;
   }
